b37ea1835f6cb496f73953ad1cb47df1f97d9964
Vusi <vusi.mosime@gmail.com>
2022-04-07 21:42:23 +0200

    Update: Fixed minor glitches

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
-// CollectNames();
+// Create user inputs to collect names upon clicking the Enter button
+enter.onclick = function () {
+  CollectNames();
+};
+// Collect names upon clicking the Submit button
+Subm_btn.onclick = function () {
+  let fName = getWinner();
 
+  let par = document.createElement("p");
 
+  let textToAdd = document.createTextNode(
+    ` The Winner of the ${titleName.value} is ${fName} `
+  );
+  par.appendChild(textToAdd);
+  resultDiv.appendChild(par);
 
+  reset_btn.hidden = false;
+  Subm_btn.hidden = true;
+};
 
-// Subm_btn.onclick = function () {
-
-//   let fName=getWinner();
-//    console.log(fName);
-//   // reset();
-//   reset_btn.hidden = false;
-//   Subm_btn.hidden = true;
-//   // reset();
-// };
-// reset_btn.onclick = function () {
-//   reset();
-//   // console.log("Hello");
-// };
-
-// let form = document.getElementsByTagName("form")[0];
-
-// form.addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   sendData();
-// });
+// Reset the program upon clicking the Reset button
+reset_btn.onclick = function () {
+  reset();
+};
